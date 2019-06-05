de2daa96754cb1e3de744c1375fb7c9d5ebe53e7
eltha9 <theadosto@gmail.com>
2019-06-06 00:30:03 +0200

    add working code of work

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,8 @@ $app->get('/', function($request, $response){
 
     $viewData['projects'] =$data;
     $viewData['style'] = "custom.css";
+    $viewData['nav_color'] = "#0E0E0E";
+
     error_log(json_encode($viewData),0);
 
     return $this->view->render($response, './pages/home.twig', $viewData);
@@ -19,6 +21,7 @@ $app->get('/about', function($request, $response){
     
     $viewData=[];
     $viewData['style']= "about.css";
+    $viewData['nav_color'] = "#0E0E0E";
 
     return $this->view->render($response, './pages/about.twig', $viewData);
 
@@ -28,8 +31,18 @@ $app->get('/about', function($request, $response){
 
 $app->get('/works/{name}', function($request, $response, $args){
     
+    $name = str_replace("_" ," ",$args['name']);
+
+    $data = $this->db->query('SELECT content FROM project_content WHERE name = "'.$name.'"')->fetch();
+    $data = json_decode(file_get_contents('../web/assets'.$data->content));
+    
     $viewData=[];
-    return $this->view->render($response, './pages/works.twig');
+    $viewData['style']= 'works.css';
+    $viewData['nav_color']= $data->nav_color;
+    $viewData['content']= $data;
+    
+
+    return $this->view->render($response, './pages/works.twig', $viewData);
 
 })->setName('works');
 
